298c8e8cb06e9334582e2abb24a484f99618e62f
quocnghia32 <88430940+quocnghia32@users.noreply.github.com>
2025-06-19 23:15:34 +0700

    Basic route to add/update/create/delete Product (#2)
    * Basic route to add/update/create/delete Product
    
    * Update case name

--- flowo-backend/internal/service/service.go
+++ flowo-backend/internal/service/service.go
@@ -13,6 +13,14 @@ type Service interface {
 	GetTodoByID(id uint) (*model.Todo, error)
 	UpdateTodo(id uint, input *dto.TodoCreate) (*model.Todo, error)
 	DeleteTodo(id uint) error
+	
+	GetAllProducts() ([]model.Product, error)
+	GetProductByID(id uint) (*model.Product, error)
+	CreateProduct(input *dto.ProductCreate) (*model.Product, error)
+	UpdateProduct(id uint, input *dto.ProductCreate) error
+	DeleteProduct(id uint) error
+	GetProductsByFlowerType(flowerType string) ([]model.Product, error)
+	GetAllFlowerTypes() ([]model.FlowerType, error)
 }
 
 type service struct {
@@ -85,4 +93,79 @@ func (s *service) DeleteTodo(id uint) error {
 	}
 
 	return s.repo.Delete(id)
+}
+func (s *service) GetAllProducts() ([]model.Product, error) {
+	products, err := s.repo.GetAllProducts()
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (s *service) GetProductByID(id uint) (*model.Product, error) {
+	product, err := s.repo.GetProductByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (s *service) CreateProduct(input *dto.ProductCreate) (*model.Product, error) {
+	product := &model.Product{
+		Name:          input.Name,
+		Description:   input.Description,
+		FlowerType:    input.FlowerType,
+		BasePrice:     input.BasePrice,
+		Status:        input.Status,
+		StockQuantity: input.StockQuantity,
+	}
+
+	err := s.repo.CreateProduct(product)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (s *service) UpdateProduct(id uint, input *dto.ProductCreate) error {
+	_, err := s.repo.GetProductByID(id)
+	if err != nil {
+		//check if the product exists
+		if err.Error() == "not found" {
+			return errors.New("product not found")
+		}
+		return err
+	}
+
+	err = s.repo.UpdateProduct(id, input)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *service) DeleteProduct(id uint) error {
+	_, err := s.repo.GetProductByID(id)
+	if err != nil {
+		return err
+	}
+	return s.repo.DeleteProduct(id)
+}
+
+func (s *service) GetProductsByFlowerType(flowerType string) ([]model.Product, error) {
+	products, err := s.repo.GetProductsByFlowerType(flowerType)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (s *service) GetAllFlowerTypes() ([]model.FlowerType, error) {
+	flowerTypes, err := s.repo.GetAllFlowerTypes()
+	if err != nil {
+		return nil, err
+	}
+	return flowerTypes, nil
 }
