182359a36645faeef936f817d241cc825d150009
Fidisk <70708171+Fidisk@users.noreply.github.com>
2025-08-03 07:57:28 +0700

    Revert "Frontend/new"

--- flowo-frontend/src/components/layout/Container.tsx
+++
@@ -1,4 +0,0 @@
-import { twMerge } from "tailwind-merge";
-export default function Container({ children, className = "" }: { children: React.ReactNode; className?: string }) {
-  return <div className={twMerge("mx-auto w-full max-w-5xl px-4", className)}>{children}</div>;
-}
