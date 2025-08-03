94dd6dfb57f7667b56fcf8c5c09575f8f8c6069c
nmquan22 <128149552+nmquan22@users.noreply.github.com>
2025-08-03 21:44:11 +0700

    complete cart with middleware

--- flowo-backend/cmd/main.go
+++ flowo-backend/cmd/main.go
@@ -160,11 +160,11 @@ func RegisterRoutes(
 	v1 := router.Group("/api/v1")
 	authCtrl.RegisterRoutes(v1, authMiddleware)
 	reviewCtrl.RegisterRoutes(v1)
-	cartCtrl.RegisterRoutes(v1)
 	pricingCtrl.RegisterRoutes(v1)
 
 	v1.Use(authMiddleware.RequireAuth())
 
+	cartCtrl.RegisterRoutes(v1)
 	orderCtrl.RegisterRoutes(v1)
 
 	logger.Init()
